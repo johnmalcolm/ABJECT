ee5dce6abef8279181c1fd22322741e611796272
John Malcolm Anderson <shmal13@gmail.com>
2023-03-28 03:19:28 +0300

    Custom ChatGPT in the playground

---
+++ playground/chatgpt-custom/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const app = express()
+app.use(bodyParser.json());
+app.use(cors());
+const port = 3080
+
+app.post('/', async(req, res) => {
+    const { message } =  req.body;
+    console.log(message);
+    const response = await openai.createCompletion({
+        model: "text-davinci-003",
+        prompt: `${message}`,
+        max_tokens: 100,
+        temperature: 0.5,
+      });
+    res.json({message:response.data.choices[0].text})
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
