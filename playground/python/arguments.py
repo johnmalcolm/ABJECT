2050ff6b00a27ce78c1c614cc69fc3a36e0c38fd
John Malcolm Anderson <shmal13@gmail.com>
2023-05-04 22:57:38 +0300

    Arguments

--- playground/python/arguments.py
+++ playground/python/arguments.py
@@ -10,3 +10,9 @@ parrot(voltage=1000000, action='VOOOOM')
 parrot(action='VOOOOM', voltage=1000000)
 parrot('a million', 'bereft of life', 'jump')
 parrot('a thousand', state='pushing up the daisies')
+
+THE THINGS BELOW WILL NOT WORK 
+# parrot() # required argument missing 
+# parrot(voltage=5.0, 'dead') # non-keyword argument after a keyword argument 
+# parrot(110, voltage=220) # duplicate value for the same argument 
+# parrot(actor='John Cleese') # unknown keyword argument
